6ac79e7844d3634cf1d8bc7ae1b52e12c641a989
Rizwan Walayat <rizwan.wallayat@gmail.com>
2024-12-18 01:59:00 +0500

    Initial Commit

---
+++ Reservations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reservations
+//
+//  Created by Techtiz PM on 18/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Techtiz PM on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
