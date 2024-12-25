08ed0a84f7b38aa3b9b431c59f1749c97f019d4e
Rizwan Walayat <rizwan.wallayat@gmail.com>
2024-12-26 01:52:01 +0500

    TextField Observing Methods

---
+++ ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  Reservations
+//
+//  Created by Techtiz PM on 26/12/2024.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
